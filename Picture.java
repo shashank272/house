ad565d86bc1387f90bee5a87a56240685de97d65
shashank272 <shashank10@gmail.com>
2019-09-09 13:05:27 -0400

    Updatepicture.java

--- Picture.java
+++ Picture.java
@@ -38,20 +38,19 @@ public class Picture
             wall.moveHorizontal(-140);
             wall.moveVertical(20);
             wall.changeSize(120);
-            wall.changeColor("blue");
             wall.makeVisible();
-
+            
             window.changeColor("black");
             window.moveHorizontal(-120);
             window.moveVertical(40);
             window.changeSize(40);
             window.makeVisible();
-
+    
             roof.changeSize(60, 180);
             roof.moveHorizontal(20);
             roof.moveVertical(-60);
             roof.makeVisible();
-
+    
             sun.changeColor("yellow");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
@@ -77,9 +76,9 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
+        wall.changeColor("green");
+        window.changeColor("blue");
+        roof.changeColor("red");
         sun.changeColor("yellow");
     }
 }
